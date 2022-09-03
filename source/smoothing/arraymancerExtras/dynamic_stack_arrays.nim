54a03c21f1d19bef6e11db92b06ff75074fc8c76
hugosenari <hugosenari@gmail.com>
2022-09-03 19:52:28 -0300

    feat(nimpkgs): update lock

--- source/smoothing/arraymancerExtras/dynamic_stack_arrays.nim
+++
@@ -1,8 +0,0 @@
-import arraymancer
-
-
-iterator zip*[T, U](a: DynamicStackArray[T], b: openarray[U]): (T, U) =
-  let len = min(a.len, b.len)
-
-  for i in 0..<len:
-    yield (a[i], b[i])
--- source/tests/tsj.nim
+++
@@ -1,9 +0,0 @@
-import unittest
-import smoothing/sj
-import arraymancer
-
-test "hsj":
-  let x = [1,2,4,8,4,2,1,8,16,12].toTensor() # not normal
-  let expected = 1.9747
-  check expected == round(hsj(x), 4)
-        
